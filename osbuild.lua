12631d9de18ab56e3296f400b0f71df237567ddc
Harald Hoyer <harald@redhat.com>
2018-11-27 15:09:08 +0100

    honor empty groups

--- osbuild.lua
+++ osbuild.lua
@@ -37,7 +37,7 @@ local function useradd(pkgname, user, group, gecko, home, shell, uid, groups)
     osbuild[pkgname]["users"][user]["shell"] = shell
     osbuild[pkgname]["users"][user]["uid"] = tonumber(uid)
 
-    if groups then
+    if groups and groups ~= "" then
         osbuild[pkgname]["users"][user]["groups"] = json:newArray(osbuild[pkgname]["users"][user]["groups"])
         for g in groups:gmatch("[^,]*") do
             table.insert(osbuild[pkgname]["users"][user]["groups"], g)
